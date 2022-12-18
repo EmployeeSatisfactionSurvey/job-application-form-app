646a5af2ae99a01f3a6ce3ce01aff46b176b423d
BOUHENNICHE <sihem.bouhenniche@inria.fr>
2022-12-18 23:38:21 +0100

    Create the project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { StylesManager, Model } from 'survey-core';
 
+import { schema } from './form.schema';
+StylesManager.applyTheme('defaultV2');
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'job-application-form-app';
+  jobApplicationForm!: Model;
+  ngOnInit(): void {
+    const form = new Model(schema);
+    this.jobApplicationForm = form;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { SurveyModule } from 'survey-angular-ui';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, SurveyModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
