b39ac2251db1aded379f45ef03a457b497b9c31e
BOUHENNICHE <sihem.bouhenniche@inria.fr>
2022-12-19 11:06:48 +0100

    Add onUploadFiles trigger

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { StylesManager, Model } from 'survey-core';
 import 'survey-core/survey.i18n';
 import { schema } from './form.schema';
+
 StylesManager.applyTheme('defaultV2');
 @Component({
   selector: 'app-root',
@@ -10,12 +11,25 @@ StylesManager.applyTheme('defaultV2');
 })
 export class AppComponent implements OnInit {
   title = 'job-application-form-app';
+
   jobApplicationForm!: Model;
+
   ngOnInit(): void {
     const form = new Model(schema);
-    this.jobApplicationForm = form;
+
+    // access to the uploaded files
+    form.onUploadFiles.add((form, options) => {
+      console.log('File uploaded');
+      var formData = new FormData();
+      options.files.forEach(function (file: any) {
+        formData.append(file.name, file);
+      });
+    });
+
     // To switch the language
     form.locale = 'fr';
     // form.locale = "en"
+
+    this.jobApplicationForm = form;
   }
 }
